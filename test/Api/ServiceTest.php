0a5f74fbb9d0679d9c442aacefe1bb0e312e2342
Warren Parad <gpg@warrenparad.net>
2022-09-30 00:05:26 +0200

    Remove unnecessarry load in unit tests.

--- test/Api/ServiceTest.php
+++ test/Api/ServiceTest.php
@@ -46,7 +46,7 @@ class ServiceTest extends \PHPUnit\Framework\TestCase
         //     throw $e;
         // }
 
-        json_decode(file_get_contents('../../vendor/composer/installed.json'), true);
+        // json_decode(file_get_contents('../../vendor/composer/installed.json'), true);
         print(implode(' ', get_defined_constants()));
 
     }
